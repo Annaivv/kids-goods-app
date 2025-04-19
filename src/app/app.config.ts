7eaf25c94ebdc3ec36729dc7af84f5d755189943
annaivv <annashalya@gmail.com>
2025-04-19 19:36:06 +0200

    feature: Firebase configuration added to appConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
 
+const firebaseConfig = {
+  apiKey: process.env['FIREBASE_API_KEY'],
+  authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
+  projectId: process.env['FIREBASE_PROJECT_ID'],
+  storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
+  messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
+  appId: process.env['FIREBASE_APP_ID'],
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+  ],
 };
